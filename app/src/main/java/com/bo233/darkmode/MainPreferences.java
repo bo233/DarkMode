d47f606e3833a2f9aef9c126374352cd98c93392
bo233 <ludingbo@hotmail.com>
2019-02-17 23:57:54 +0800

    time picker v1

--- app/src/main/java/com/bo233/darkmode/MainPreferences.java
+++ app/src/main/java/com/bo233/darkmode/MainPreferences.java
@@ -20,9 +20,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
+import cn.addapp.pickers.picker.TimePicker;
+
 public class MainPreferences extends PreferenceFragment {
     private CheckBoxPreference openPreference;
     private CheckBoxPreference setByAppPreference;
+    private CheckBoxPreference timePreference;
     private Properties properties;
     public final static File PROP_FILE=new File("/sdcard/Android/data/com.bo233.darkmode/settings.ini");
 //    public final static File PROP_FILE = new File(getExternalFilesDir()+"settings.ini");
@@ -33,6 +36,7 @@ public class MainPreferences extends PreferenceFragment {
         addPreferencesFromResource(R.xml.main_preferences);
         openPreference = (CheckBoxPreference) findPreference("key_switch");
         setByAppPreference = (CheckBoxPreference) findPreference("setting_by_apps");
+        timePreference = (CheckBoxPreference) findPreference("time_switch");
         properties=new Properties();
 
         try {
@@ -74,6 +78,37 @@ public class MainPreferences extends PreferenceFragment {
                 Intent intent = new Intent(getActivity(), SettingByAppsActivity.class);
                 startActivity(intent);
                 return false;
+            }
+        });
+
+        timePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean timeSwitch = (boolean)newValue;
+                properties.setProperty("time_switch",timeSwitch+"");
+                try {
+                    properties.store(new FileOutputStream(PROP_FILE),comment);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(timeSwitch){
+                    TimePicker picker = new TimePicker(getActivity(), TimePicker.HOUR_24);
+                    picker.setRangeStart(0, 0);
+                    picker.setRangeEnd(23, 59);
+                    picker.setTopLineVisible(false);
+                    picker.setLineVisible(false);
+                    picker.setWheelModeEnable(false);
+                    picker.setOnTimePickListener(new TimePicker.OnTimePickListener() {
+                        @Override
+                        public void onTimePicked(String hour, String minute) {
+
+                        }
+                    });
+                    picker.show();
+
+                }
+                return true;
             }
         });
 
---
+++ app/src/main/java/com/bo233/darkmode/util/DoubleTimePicker.java
@@ -0,0 +1,7 @@
+package com.bo233.darkmode.util;
+
+import cn.addapp.pickers.picker.TimePicker;
+
+public class DoubleTimePicker {
+
+}
